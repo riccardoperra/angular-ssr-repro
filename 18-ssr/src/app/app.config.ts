e15372e910bc007e9aa95fb780f88091078c7a8a
riccardoperra <riccardo.perra@icloud.com>
2024-10-25 21:10:53 +0200

    with transfer state

--- 18-ssr/src/app/app.config.ts
+++ 18-ssr/src/app/app.config.ts
@@ -1,15 +1,29 @@
-import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {ApplicationConfig, inject, makeStateKey, PLATFORM_ID, provideZoneChangeDetection, TransferState} from '@angular/core';
 import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
 import {provideClientHydration} from '@angular/platform-browser';
-import {provideExternalClient} from './ssr.providers';
+import {API_URL, provideExternalClient} from './ssr.providers';
+import { isPlatformServer } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
     provideClientHydration(),
-    provideExternalClient()
+    provideExternalClient(),
+
+    {
+      provide: API_URL,
+      useFactory: () => {
+        const platform = inject(PLATFORM_ID);
+        const ts = inject(TransferState);
+        const key = makeStateKey<string>('apiUrl');
+        if (isPlatformServer(platform)) {
+          ts.set(key, inject(API_URL, {skipSelf: true}))
+        }
+        return ts.get(key, '');
+      }
+    }
   ]
 };
